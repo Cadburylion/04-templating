9a8c7dcf245df3b7bf478ddd0e6f5c456464081c
Matthew Parker <cadburylion@outlook.com>
2017-05-11 13:21:14 -0700

    finished third TODO and got blogs to post to DOM using Handlebars

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -16,6 +16,12 @@ Article.prototype.toHtml = function() {
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
 
+  var template = $('#article-template').html(); // gets template from html
+
+  var templateRender = Handlebars.compile(template); // compiles template using Handlebars
+
+  return templateRender(this); // returns rendered template
+
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
